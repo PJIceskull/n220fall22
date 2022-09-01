d48e8dde574dc6231bdcbf3f3bd3da98035306aa
Pierce Issah <PJIceskull2003@gmail.com>
2022-08-31 20:28:17 -0400

    created canvas

--- 2_Animation, Variables, and Conditionals/canvasExample/drawShapes/app.js
+++ 2_Animation, Variables, and Conditionals/canvasExample/drawShapes/app.js
@@ -1,7 +1,10 @@
+// use setup to draw canvas
 function setup() {
-
+    createCanvas(480, 240);
+    // background(205);
 }
 
+// use draw to make shapes or backgrounds
 function draw() {
-
+    background(205);
 }
