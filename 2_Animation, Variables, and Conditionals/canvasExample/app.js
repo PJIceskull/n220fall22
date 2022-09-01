64674adec521862f5901d5ac07a3e23ace21c9d6
Pierce Issah <PJIceskull2003@gmail.com>
2022-08-31 20:23:19 -0400

    coded to draw a point/pixel

--- 2_Animation, Variables, and Conditionals/canvasExample/app.js
+++ 2_Animation, Variables, and Conditionals/canvasExample/app.js
@@ -1,8 +1,15 @@
 function setup() {
-    createCanvas(800, 600);
+    // sizes
+    // createCanvas(800, 600);
+    createCanvas(480, 120);
     // create Gray background color for canvas
-    background(200);
+    // background(200);
 }
 function draw() {
-
+    background(204);
+    point(240, 60);
+    point(280, 60);
+    // bottom Points 
+    point(240, 90);
+    point(280, 90);
 }
