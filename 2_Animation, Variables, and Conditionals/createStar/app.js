8b2705456f5ff94dd46f957ed1d44a57afc850b8
Pierce Issah <PJIceskull2003@gmail.com>
2022-08-31 22:45:55 -0400

    draw two lines

--- 2_Animation, Variables, and Conditionals/createStar/app.js
+++ 2_Animation, Variables, and Conditionals/createStar/app.js
@@ -1,3 +1,5 @@
+fillRed = 255, 0, 0;
+
 function setup() {
     createCanvas(500, 500);
 }
@@ -5,4 +7,32 @@ function setup() {
 function draw() {
     // background color for Canvas
     background(200);
+    // 
+    point(50, 125)
+    point(150, 125)
+    point(250, 125)
+    point(350, 125)
+    point(450, 125)
+    // 
+    point(50, 250)
+    point(150, 250)
+    point(250, 250)
+    point(350, 250)
+    point(450, 250)
+    // 
+    point(50, 375)
+    point(150, 375)
+    point(250, 375)
+    point(350, 375)
+    point(450, 375)
+    //
+
+    // line(x1, y1, x2, y2)
+
+    // Star
+    // line 1
+    line(250, 125, 200, 250)
+    // line 2
+    line(250, 125, 200, 350)
+
 }
