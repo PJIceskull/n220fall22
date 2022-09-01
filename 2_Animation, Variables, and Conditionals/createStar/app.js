a9409dc6b2087e3f08bfed8b37d2200046f30a4b
Pierce Issah <PJIceskull2003@gmail.com>
2022-08-31 22:54:21 -0400

    Added Comments

--- 2_Animation, Variables, and Conditionals/createStar/app.js
+++ 2_Animation, Variables, and Conditionals/createStar/app.js
@@ -5,25 +5,24 @@ function setup() {
 function draw() {
     // background color for Canvas
     background(200);
-    // 
-    point(50, 125)
-    point(150, 125)
-    point(250, 125)
-    point(350, 125)
-    point(450, 125)
-    // 
-    point(50, 250)
-    point(150, 250)
-    point(250, 250)
-    point(350, 250)
-    point(450, 250)
-    // 
-    point(50, 375)
-    point(150, 375)
-    point(250, 375)
-    point(350, 375)
-    point(450, 375)
-    //
+    // Grid for labeling out Points 
+    // point(50, 125)
+    // point(150, 125)
+    // point(250, 125)
+    // point(350, 125)
+    // point(450, 125)
+    // // 
+    // point(50, 250)
+    // point(150, 250)
+    // point(250, 250)
+    // point(350, 250)
+    // point(450, 250)
+    // // 
+    // point(50, 375)
+    // point(150, 375)
+    // point(250, 375)
+    // point(350, 375)
+    // point(450, 375)
 
     // line(x1, y1, x2, y2)
 
