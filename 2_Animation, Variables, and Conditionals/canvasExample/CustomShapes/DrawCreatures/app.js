ce3636648f56fcef0e560827fd8cc633e8db9906
Pierce Issah <PJIceskull2003@gmail.com>
2022-09-05 15:16:07 -0400

    Added code to Draw right Creature

--- 2_Animation, Variables, and Conditionals/canvasExample/CustomShapes/DrawCreatures/app.js
+++ 2_Animation, Variables, and Conditionals/canvasExample/CustomShapes/DrawCreatures/app.js
@@ -3,9 +3,10 @@ function setup() {
 }
 
 function draw() {
-    background(204);
+    background(200);
 
     // Left Creature
+    fill(255) // White Fill Color
     beginShape();
     vertex(50, 120);
     vertex(100, 90);
@@ -16,9 +17,23 @@ function draw() {
     vertex(200, 90);
     vertex(140, 100);
     vertex(130, 120);
-    endshape();
+    endShape();
+    fill(0); // Black Fill Color
+    ellipse(155, 60, 8, 8);
 
     // Right Creature
-    // beginShape();
-    // endshape();
+    fill(255);
+    beginShape();
+    vertex(370, 120);
+    vertex(360, 90);
+    vertex(290, 80);
+    vertex(340, 70);
+    vertex(280, 50);
+    vertex(420, 10);
+    vertex(390, 50);
+    vertex(410, 90);
+    vertex(460, 120);
+    endShape();
+    fill(0);
+    ellipse(345, 50, 10, 10);
 }
