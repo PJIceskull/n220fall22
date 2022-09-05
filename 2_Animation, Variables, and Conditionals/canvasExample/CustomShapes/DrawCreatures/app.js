09a75da78da5477caedafabbcb3fbcdb26d411a0
Pierce Issah <PJIceskull2003@gmail.com>
2022-09-05 15:07:22 -0400

    Start Drawing Shape for left Creature

--- 2_Animation, Variables, and Conditionals/canvasExample/CustomShapes/DrawCreatures/app.js
+++ 2_Animation, Variables, and Conditionals/canvasExample/CustomShapes/DrawCreatures/app.js
@@ -6,8 +6,19 @@ function draw() {
     background(204);
 
     // Left Creature
-
+    beginShape();
+    vertex(50, 120);
+    vertex(100, 90);
+    vertex(110, 60);
+    vertex(80, 20);
+    vertex(210, 60);
+    vertex(160, 80);
+    vertex(200, 90);
+    vertex(140, 100);
+    vertex(130, 120);
+    endshape();
 
     // Right Creature
-
+    // beginShape();
+    // endshape();
 }
