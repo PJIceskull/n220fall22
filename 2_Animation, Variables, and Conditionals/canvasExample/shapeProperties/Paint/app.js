8f676faa3c3787e1850f2efa9f54756ca26e6ef9
Pierce Issah <PJIceskull2003@gmail.com>
2022-09-05 14:40:34 -0400

    Added a example with Transparency

--- 2_Animation, Variables, and Conditionals/canvasExample/shapeProperties/Paint/app.js
+++ 2_Animation, Variables, and Conditionals/canvasExample/shapeProperties/Paint/app.js
@@ -15,12 +15,21 @@ function draw() {
     // fill(102); // Dark Gray
     // ellipse(268, 118, 200, 200); // Dark Gray Circle
 
-    // Draw with Color
-    background(0, 26, 51); // Dark Blue Background
-    fill(255, 0, 0); // Red Fill Color
+    // // Draw with Color
+    // background(0, 26, 51); // Dark Blue Background
+    // fill(255, 0, 0); // Red Fill Color
+    // ellipse(132, 82, 200, 200); // Red Circle
+    // fill(0, 255, 0); // Green Fill Color
+    // ellipse(228, -16, 200, 200); // Green Circle
+    // fill(0, 0, 255); // Blue Fill Color
+    // ellipse(268, 118, 200, 200); // Blue Circle
+
+    // Set Transparency 
+    background(204, 226, 225); // Light Blue Background
+    fill(255, 0, 0, 160); // Red Fill Color
     ellipse(132, 82, 200, 200); // Red Circle
-    fill(0, 255, 0); // Green Fill Color
+    fill(0, 255, 0, 160); // Green Fill Color
     ellipse(228, -16, 200, 200); // Green Circle
-    fill(0, 0, 255); // Blue Fill Color
+    fill(0, 0, 255, 160); // Blue Fill Color
     ellipse(268, 118, 200, 200); // Blue Circle
 }
