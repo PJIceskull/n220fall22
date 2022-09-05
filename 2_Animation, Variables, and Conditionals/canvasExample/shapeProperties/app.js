dddbbd19b31f7e44182acad9c00d9dce229e9c27
Pierce Issah <PJIceskull2003@gmail.com>
2022-09-05 13:48:22 -0400

    Add Stroke Weight

--- 2_Animation, Variables, and Conditionals/canvasExample/shapeProperties/app.js
+++ 2_Animation, Variables, and Conditionals/canvasExample/shapeProperties/app.js
@@ -1,7 +1,17 @@
 function setup() {
-    createCanvas(480, 120)
+    createCanvas(480, 120);
 }
 
 function draw() {
-
+    background(204);
+    // Setting Stroke Weight
+    strokeWeight(1);
+    ellipse(75, 60, 90, 90);
+    // Set Stroke Weight to 8 pixels
+    strokeWeight(8);
+    ellipse(175, 60, 90, 90);
+    ellipse(279, 60, 90, 90);
+    // Set Stroke Weight to 20 pixels
+    strokeWeight(20);
+    ellipse(389, 60, 90, 90);
 }
