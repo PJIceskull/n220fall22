daa024aed7eae5a85381ce1d7808fc4003a52b1c
Pierce Issah <PJIceskull2003@gmail.com>
2022-09-05 17:45:10 -0400

    Created and Add for Loop Function

--- 2_Animation, Variables, and Conditionals/Variables/Repetition/app.js
+++ 2_Animation, Variables, and Conditionals/Variables/Repetition/app.js
@@ -4,13 +4,18 @@ function setup() {
 }
 
 function draw() {
-    background(200)
-    // Line Pattern
-    line(20, 40, 80, 80);
-    line(80, 40, 140, 80);
-    line(140, 40, 200, 80);
-    line(200, 40, 260, 80);
-    line(260, 40, 320, 80);
-    line(320, 40, 380, 80);
-    line(380, 40, 440, 80);
+    background(200);
+    // // Line Pattern
+    // line(20, 40, 80, 80);
+    // line(80, 40, 140, 80);
+    // line(140, 40, 200, 80);
+    // line(200, 40, 260, 80);
+    // line(260, 40, 320, 80);
+    // line(320, 40, 380, 80);
+    // line(380, 40, 440, 80);
+
+    // For Loop Function
+    for (var i = 20; i < 400; i += 60) {
+        line(i, 40, i + 60, 80);
+    } // Code inside {} is called a block
 }
