fa12838a22af4cd1044d28b1883d901d39880ff4
Pierce Issah <PJIceskull2003@gmail.com>
2022-09-05 14:37:13 -0400

    Added bg and Circles with Colors

--- 2_Animation, Variables, and Conditionals/canvasExample/shapeProperties/Paint/app.js
+++ 2_Animation, Variables, and Conditionals/canvasExample/shapeProperties/Paint/app.js
@@ -1,13 +1,26 @@
 function setup() {
     createCanvas(480, 120);
+
+    // Draw with Color
+    noStroke();
 }
 
 function draw() {
-    background(0); // Black 
-    fill(204); // Light Gray
-    ellipse(132, 82, 200, 200); // Light Gray Circle
-    fill(153); // Medium Gray
-    ellipse(228, -16, 200, 200); // Medium Gray Circle
-    fill(102); // Dark Gray
-    ellipse(268, 118, 200, 200); // Dark Gray Circle
+    // // Painting with Grays
+    // background(0); // Black 
+    // fill(204); // Light Gray
+    // ellipse(132, 82, 200, 200); // Light Gray Circle
+    // fill(153); // Medium Gray
+    // ellipse(228, -16, 200, 200); // Medium Gray Circle
+    // fill(102); // Dark Gray
+    // ellipse(268, 118, 200, 200); // Dark Gray Circle
+
+    // Draw with Color
+    background(0, 26, 51); // Dark Blue Background
+    fill(255, 0, 0); // Red Fill Color
+    ellipse(132, 82, 200, 200); // Red Circle
+    fill(0, 255, 0); // Green Fill Color
+    ellipse(228, -16, 200, 200); // Green Circle
+    fill(0, 0, 255); // Blue Fill Color
+    ellipse(268, 118, 200, 200); // Blue Circle
 }
