6c5d1748717901e62f2bb754ec63d53b873981b8
Pierce Issah <PJIceskull2003@gmail.com>
2022-09-05 16:29:56 -0400

    add p5.js variables

--- 2_Animation, Variables, and Conditionals/Variables/FirstVariables/app.js
+++ 2_Animation, Variables, and Conditionals/Variables/FirstVariables/app.js
@@ -14,7 +14,12 @@ function setup() {
 
 function draw() {
     background(200);
-    ellipse(75, y, d, d); // Left
-    ellipse(175, y, d, d); // Middle
-    ellipse(275, y, d, d); // Right
+    // // Testing out Variables
+    // ellipse(75, y, d, d); // Left
+    // ellipse(175, y, d, d); // Middle
+    // ellipse(275, y, d, d); // Right
+
+    // p5.js Variables
+    line(0, 0, width, height);
+    line(width, 0, 0, height);
 }
