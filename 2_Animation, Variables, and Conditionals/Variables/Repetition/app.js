291fccbe1ada7abf691c2ff428997b1b8164f998
Pierce Issah <PJIceskull2003@gmail.com>
2022-09-07 12:09:38 -0400

    Added more examples using for Loop

--- 2_Animation, Variables, and Conditionals/Variables/Repetition/app.js
+++ 2_Animation, Variables, and Conditionals/Variables/Repetition/app.js
@@ -1,6 +1,7 @@
 function setup() {
     createCanvas(480, 120)
-    strokeWeight(8);
+    // strokeWeight(8);
+    strokeWeight(2);
 }
 
 function draw() {
@@ -14,8 +15,13 @@ function draw() {
     // line(320, 40, 380, 80);
     // line(380, 40, 440, 80);
 
-    // For Loop Function
-    for (var i = 20; i < 400; i += 60) {
+    // // For Loop Function
+    // for (var i = 20; i < 400; i += 60) {
+    //     line(i, 40, i + 60, 80);
+    // } // Code inside {} is called a block
+
+    // Flexing your Loop Muscles
+    for (var i = 20; i < 400; i += 8) {
         line(i, 40, i + 60, 80);
-    } // Code inside {} is called a block
+    }
 }
