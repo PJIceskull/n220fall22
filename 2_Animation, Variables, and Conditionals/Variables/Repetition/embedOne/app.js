8beced49532915e1cd2ef04d5c830a021047da51
Pierce Issah <PJIceskull2003@gmail.com>
2022-09-07 15:00:02 -0400

    Add embedded loop in draw function

--- 2_Animation, Variables, and Conditionals/Variables/Repetition/embedOne/app.js
+++ 2_Animation, Variables, and Conditionals/Variables/Repetition/embedOne/app.js
@@ -1,7 +1,15 @@
 function setup() {
     createCanvas(480, 120)
+    noStroke();
 }
 
 function draw() {
-
+    background(200)
+    // loop
+    for (var y = 0; y <= height; y += 40) {
+        for (var x = 0; y <= width; x += 40) {
+            fill(115, 140)
+            ellipse(x, y, 40, 40)
+        }
+    }
 }
