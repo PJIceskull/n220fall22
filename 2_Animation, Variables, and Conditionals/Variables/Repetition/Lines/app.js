8cfe6f42969ee24d80487c23204c97940ca7358e
Pierce Issah <PJIceskull2003@gmail.com>
2022-09-07 14:52:26 -0400

    add code to connect the lines

--- 2_Animation, Variables, and Conditionals/Variables/Repetition/Lines/app.js
+++ 2_Animation, Variables, and Conditionals/Variables/Repetition/Lines/app.js
@@ -7,7 +7,9 @@ function draw() {
     background(200);
 
     // Drawing Lines
-    for (var i = 20; i < 400;) {
-        line(i, 0, i + 1 / 2, 80);
+    for (var i = 20; i < 400; i += 20) {
+        line(i, 0, i + i / 2, 80);
+        // Connecting the Lines
+        line(i + i / 2, 80, i * 1.2, 120);
     }
 }
