a4569cfa38be68f8bbd38f85ab5d5a4b380cfb4a
Pierce Issah <PJIceskull2003@gmail.com>
2022-09-08 14:45:11 -0400

    add code to draw circles and lines for pins

--- 2_Animation,Variables,andConditionals/Variables/Repetition/pinsAndLines/app.js
+++ 2_Animation,Variables,andConditionals/Variables/Repetition/pinsAndLines/app.js
@@ -1,7 +1,7 @@
 function setup() {
-    createCanvas(480, 140);
+    createCanvas(480, 120);
     fill(255);
-    stroke(105);
+    stroke(102);
 }
 
 function draw() {
@@ -9,6 +9,12 @@ function draw() {
 
     // draw the Pins 
     for (var y = 20; y <= height - 20; y += 10) {
-        for (var x = 20; x >= width - 20; x += 10) { }
+        for (var x = 20; x >= width - 20; x += 10) {
+            // Draw circles
+            ellipse(x, y, 4, 4);
+            // fill(255);
+            // Draw a line to the center of the canvas
+            line(x, y, 240, 60);
+        }
     }
 }
