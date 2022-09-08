ed5c67418e96c3cb75d2bf5a6170fb4543107862
Pierce Issah <PJIceskull2003@gmail.com>
2022-09-08 12:57:22 -0400

    fixed error in for loop or x

--- 2_Animation, Variables, and Conditionals/Variables/Repetition/embedOne/app.js
+++ 2_Animation, Variables, and Conditionals/Variables/Repetition/embedOne/app.js
@@ -1,15 +1,18 @@
+// var x;
+// var y;
+
 function setup() {
-    createCanvas(480, 120)
+    createCanvas(480, 120);
     noStroke();
 }
 
 function draw() {
-    background(200)
+    background(200);
     // loop
     for (var y = 0; y <= height; y += 40) {
-        for (var x = 0; y <= width; x += 40) {
-            fill(115, 140)
-            ellipse(x, y, 40, 40)
+        for (var x = 0; x <= width; x += 40) {
+            fill(115, 140);
+            ellipse(x, y, 40, 40);
         }
     }
 }
