c0704aaa03d48b48302b497d2e3a795da0e5ca42
Pierce Issah <PJIceskull2003@gmail.com>
2022-09-08 14:53:49 -0400

    add forLoops for x and y coords

--- 2_Animation,Variables,andConditionals/Variables/Repetition/halftoneDots/app.js
+++ 2_Animation,Variables,andConditionals/Variables/Repetition/halftoneDots/app.js
@@ -3,4 +3,8 @@ function setup() {
 }
 function draw() {
     background(0);
+    // Draw dots
+    for (var y = 32; y <= height; y += 8) {
+        for (var x = 12; x <= width; x += 15) { }
+    }
 }
