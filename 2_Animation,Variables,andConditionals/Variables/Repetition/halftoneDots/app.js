5f9fbb44aa8f33a34580066a4dfa9a770c70a186
Pierce Issah <PJIceskull2003@gmail.com>
2022-09-08 14:54:49 -0400

    draw circles

--- 2_Animation,Variables,andConditionals/Variables/Repetition/halftoneDots/app.js
+++ 2_Animation,Variables,andConditionals/Variables/Repetition/halftoneDots/app.js
@@ -5,6 +5,8 @@ function draw() {
     background(0);
     // Draw dots
     for (var y = 32; y <= height; y += 8) {
-        for (var x = 12; x <= width; x += 15) { }
+        for (var x = 12; x <= width; x += 15) {
+            ellipse(x + y, y, 16 - y / 10.0, 16 - y / 10.0);
+        }
     }
 }
