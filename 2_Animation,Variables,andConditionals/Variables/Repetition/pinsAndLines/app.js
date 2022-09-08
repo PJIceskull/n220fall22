f3e1e4e2dd99f1ecab8d4da6d924a21ca0bc3bf6
Pierce Issah <PJIceskull2003@gmail.com>
2022-09-08 14:47:01 -0400

    fixed error in x forLoop with wrong sign

--- 2_Animation,Variables,andConditionals/Variables/Repetition/pinsAndLines/app.js
+++ 2_Animation,Variables,andConditionals/Variables/Repetition/pinsAndLines/app.js
@@ -9,7 +9,7 @@ function draw() {
 
     // draw the Pins 
     for (var y = 20; y <= height - 20; y += 10) {
-        for (var x = 20; x >= width - 20; x += 10) {
+        for (var x = 20; x <= width - 20; x += 10) {
             // Draw circles
             ellipse(x, y, 4, 4);
             // fill(255);
