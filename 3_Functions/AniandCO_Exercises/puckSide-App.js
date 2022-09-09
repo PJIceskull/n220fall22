a591a6d1a73ed334a767c42cd6e9e2e7ee81fd26
Pierce Issah <PJIceskull2003@gmail.com>
2022-09-08 20:06:28 -0400

    fixed error in script link

--- 3_Functions/AniandCO_Exercises/puckSide-App.js
+++ 3_Functions/AniandCO_Exercises/puckSide-App.js
@@ -1,9 +1,9 @@
 function setup() {
     // Create a canvas for the program
     createCanvas(400, 300); // Creates the width and height of our canvas
-    background(205); // set background color to a gray color
+
 }
 
 function draw() {
-
+    background(205); // set background color to a gray color
 }
