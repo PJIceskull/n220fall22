ad42a156af3ca66810d8fe23a347dd0f7846063a
Pierce Issah <PJIceskull2003@gmail.com>
2022-09-08 22:26:27 -0400

    added circle at the center of the canvas

--- 3_Functions/AniandCO_Exercises/worldWarp-App.js
+++ 3_Functions/AniandCO_Exercises/worldWarp-App.js
@@ -4,20 +4,20 @@ function setup() {
 
 function draw() {
     background(255);
+    // Test
     // Draw lines to map out the boundaries.
     // line 1
     line(0, 0, 0, 600)
     // line 2 
     line(800, 0, 800, 600)
     // Draw Circle and track it to mouse
-    ellipse(mouseX, mouseY, 40, 40);
-
+    ellipse(mouseX, mouseY, 50, 50);
     // text the Boundaries
     // if mouse's x position is 800
     if (mouseX >= 800) { // x is greater 800 pixels,
         fill(0); // make fill color black
     } else {
-        fill(255)
+        fill(255) // make fill white
     }
-
+    ellipse(400, 300, 50, 50);
 }
