f1b22cc5d7cd0be0673fd8badab4af5c6bced5f6
Pierce Issah <PJIceskull2003@gmail.com>
2022-09-08 21:46:44 -0400

    code lines to show the canvas boundaries

--- 3_Functions/AniandCO_Exercises/worldWarp-App.js
+++ 3_Functions/AniandCO_Exercises/worldWarp-App.js
@@ -1,7 +1,12 @@
 function setup() {
-    createCanvas(800, 600);
+    createCanvas(800, 600); // canvas(width,height)
 }
 
 function draw() {
     background(255);
+    // Draw lines to map out the boundaries.
+    // line 1
+    line(0, 0, 0, 600)
+    // line 2 
+    line(800, 0, 800, 600)
 }
