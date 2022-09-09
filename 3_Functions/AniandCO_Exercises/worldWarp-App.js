7b14e5720b2d07db1aafe159cb93e5f22df667bd
Pierce Issah <PJIceskull2003@gmail.com>
2022-09-08 22:42:20 -0400

    write if statement to reset x position

--- 3_Functions/AniandCO_Exercises/worldWarp-App.js
+++ 3_Functions/AniandCO_Exercises/worldWarp-App.js
@@ -8,23 +8,26 @@ function setup() {
 
 function draw() {
     background(255);
-    // Test
-    // Draw lines to map out the boundaries.
-    // line 1
-    line(0, 0, 0, 600)
-    // line 2 
-    line(800, 0, 800, 600)
-    // Draw Circle and track it to mouse
-    ellipse(mouseX, mouseY, 50, 50);
-    // text the Boundaries
-    // if mouse's x position is 800
-    if (mouseX >= 800) { // x is greater 800 pixels,
-        fill(0); // make fill color black
-    } else {
-        fill(255) // make fill white
-    }
+    // // Test
+    // // Draw lines to map out the boundaries.
+    // // line 1
+    // line(0, 0, 0, 600)
+    // // line 2 
+    // line(800, 0, 800, 600)
+    // // Draw Circle and track it to mouse
+    // ellipse(mouseX, mouseY, 50, 50);
+    // // text the Boundaries
+    // // if mouse's x position is 800
+    // if (mouseX >= 800) { // x is greater 800 pixels,
+    //     fill(0); // make fill color black
+    // } else {
+    //     fill(255) // make fill white
+    // }
 
-    x = x + speed;
-    ellipse(x, 250, 75, 75);
-    // ellipse(400, 250, 100, 100);
+    x = x + speed; // x position/movement will increase by 5 pixels
+    ellipse(x, 250, 75, 75); // draw ellipse
+
+    if (x >= 800) {
+        x = 0; // reset x value/position back to zero
+    }
 }
