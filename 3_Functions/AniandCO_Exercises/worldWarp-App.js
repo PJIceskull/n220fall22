0a077c3e0bdd8f3ec17927558cf9bcb217aa23d0
Pierce Issah <PJIceskull2003@gmail.com>
2022-09-08 22:08:57 -0400

    add code to change fill color position is 800 px

--- 3_Functions/AniandCO_Exercises/worldWarp-App.js
+++ 3_Functions/AniandCO_Exercises/worldWarp-App.js
@@ -9,4 +9,15 @@ function draw() {
     line(0, 0, 0, 600)
     // line 2 
     line(800, 0, 800, 600)
+    // Draw Circle and track it to mouse
+    ellipse(mouseX, mouseY, 40, 40);
+
+    // text the Boundaries
+    // if mouse's x position is 800
+    if (mouseX >= 800) { // x is greater 800 pixels,
+        fill(0); // make fill color black
+    } else {
+        fill(255)
+    }
+
 }
