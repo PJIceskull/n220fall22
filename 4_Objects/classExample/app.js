c8e04412dc92b1f4130e46f1f7dea784e38e283f
Pierce Issah <PJIceskull2003@gmail.com>
2022-09-13 10:45:41 -0400

    Upload files to project dir

---
+++ 4_Objects/classExample/app.js
@@ -0,0 +1,23 @@
+// Variables 
+// let key allows for more scope
+let name = "Travis"; // strings
+let age = "36"; // numerical data
+let hungry = true; // boolean
+
+// Object Variable
+let me = {
+    // Varibles and Properties
+    name: "Travis",
+    age: 36,
+    hungry: true
+}
+
+function setup() {
+    createCanvas(800, 600)
+}
+function draw() {
+
+}
+function drawface(x, y) {
+
+}
