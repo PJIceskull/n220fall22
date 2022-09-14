f4c4719063b933bca20198246653d07dcef21fe2
Pierce Issah <PJIceskull2003@gmail.com>
2022-09-14 13:13:57 -0400

    print i'm runing and starting

--- 2_Animation,Variables,andConditionals/Response/printAndDraw/app.js
+++ 2_Animation,Variables,andConditionals/Response/printAndDraw/app.js
@@ -1,11 +1,13 @@
 function setup() {
     createCanvas(480, 120);
+    print("I'm starting");
 }
 
 function draw() {
     background(200)
 
     // Displays the frame count to the console
-    print("I'm Drawing");
-    print(frameCount);
+    // print("I'm Drawing");
+    // print(frameCount);
+    print("I'm Running")
 }
