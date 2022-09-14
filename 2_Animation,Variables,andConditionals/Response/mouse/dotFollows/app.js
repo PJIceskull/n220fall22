4b13d5c5e9e8cc2285414f42d13fafb75dbd4182
Pierce Issah <PJIceskull2003@gmail.com>
2022-09-14 13:29:09 -0400

    add line with pmouse positions

--- 2_Animation,Variables,andConditionals/Response/mouse/dotFollows/app.js
+++ 2_Animation,Variables,andConditionals/Response/mouse/dotFollows/app.js
@@ -8,5 +8,7 @@ function setup() {
 
 function draw() {
     background(200); // background clears canvas
-    ellipse(mouseX, mouseY, 20, 20);
+    // ellipse(mouseX, mouseY, 20, 20);
+    // line
+    line(mouseX, mouseY, pmouseX, pmouseY); // pmouseX and pmouseY stores the position of the mouse in the previous frame
 }
