ad9d815b9c6b83853cefbb1d3fb46dc47d03425f
Pierce Issah <PJIceskull2003@gmail.com>
2022-09-20 11:16:45 -0400

    add styles to when elText is clicked

---
+++ 5_DomManipulation_and_CSS/inClass/example/app.js
@@ -0,0 +1,10 @@
+// Variables
+let elText = document.getElementById("elTest");
+
+
+
+function changeText() {
+    elText.innerHTML = "COOL STORY DUDE!";
+    elText.style.color = "#fff";
+    elText.style.backgroundColor = "#00FFAA";
+}
