a4a6571e0d6f5f97b97053162413507ffe9cfe01
Pierce Issah <PJIceskull2003@gmail.com>
2022-09-20 11:20:25 -0400

    add js code targeting the style of elTest

--- 5_DomManipulation_and_CSS/inClass/example/app.js
+++ 5_DomManipulation_and_CSS/inClass/example/app.js
@@ -1,6 +1,11 @@
 // Variables
 let elText = document.getElementById("elTest");
 
+elText.style.backgroundColor = "#000";
+elText.style.color = "#FFcc3c";
+elText.style.width = "300px";
+// "300px" + 2 // "300px2"
+
 
 
 function changeText() {
