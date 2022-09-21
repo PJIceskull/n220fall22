737346ced6d772ab45040a5faa7359a2cafd3e11
Pierce Issah <PJIceskull2003@gmail.com>
2022-09-21 12:46:04 -0400

    add comments for code

--- 5_DomManipulation_and_CSS/inClass/drawTV/app.js
+++ 5_DomManipulation_and_CSS/inClass/drawTV/app.js
@@ -5,12 +5,20 @@ function setup() {
     createCanvas(800, 600);
 }
 
+let tv = {
+    x: 230, y: 200, l: 400, w: 250, // TV Sizes
+    tvColor: "#000000", // TV Color
+    screenColor: "#FFFFFF",
+    power: true,
+    // antennaX: 15, antennaY: 15,
+}
 
 function drawTV() {
-    fill(0);
-    rect(230, 200, 400, 250);
-    fill(255);
+    fill(tv.tvColor); // Color of the TV will be black
+    rect(tv.x, tv.y, tv.l, tv.w); // Use Variables for the TV sizes
+    fill(tv.screenColor); // Set the inside color to white
     rect(250, 220, 360, 210);
+    // line(tv.x, tv.y,)
 }
 
 function draw() {
