69087c2560121cd34d677a974c07e55c13ee6343
Pierce Issah <PJIceskull2003@gmail.com>
2022-09-21 12:27:49 -0400

    Add code to draw a tv screen

---
+++ 5_DomManipulation_and_CSS/inClass/drawTV/app.js
@@ -0,0 +1,18 @@
+// Variables
+// Create a Variable that targets the class for your tv
+
+function setup() {
+    createCanvas(800, 600);
+}
+
+
+function drawTV() {
+    fill(0);
+    rect(230, 200, 400, 250);
+    fill(255);
+    rect(250, 220, 360, 210);
+}
+
+function draw() {
+    drawTV();
+}
