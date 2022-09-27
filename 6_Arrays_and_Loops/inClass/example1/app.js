b8da27347fdf56c279b66070e077276c8bcd5277
Pierce Issah <PJIceskull2003@gmail.com>
2022-09-27 11:29:46 -0400

    create div and style it through javascript

--- 6_Arrays_and_Loops/inClass/example1/app.js
+++ 6_Arrays_and_Loops/inClass/example1/app.js
@@ -2,11 +2,12 @@
 let colors = ["#18206f", "#17255A", "#F5E2C8", "#D88373", "#BD1E1E", "red", "purple"];
 
 // for(var i = 0; i < colors.length; i++) {
-for (var i = 0; i < 6; i++) {
+for (var i = 0; i < 7; i++) {
     let aDiv = document.createElement("div");
     aDiv.innerHTML = colors[i];
     aDiv.style.width = "100px";
     aDiv.style.height = "100px";
+    aDiv.style.color = "#ffffff";
     aDiv.style.backgroundColor = colors[i];
     document.body.appendChild(aDiv);
 }
