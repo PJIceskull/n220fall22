1484368043047f7bb26a09d032de0c60799af81a
Pierce Issah <PJIceskull2003@gmail.com>
2022-09-27 11:23:16 -0400

    create files for project

---
+++ 6_Arrays_and_Loops/inClass/example1/app.js
@@ -0,0 +1,7 @@
+// Variables
+let colors = ["#042a2b", "red", "purple"];
+
+// for(var i = 0; i < colors.length; i++) {
+for (var i = 0; i < 6; i++) {
+    let aDiv = document.createElement("div");
+}
