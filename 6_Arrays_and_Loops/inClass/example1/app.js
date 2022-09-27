dca72328b514b1815709b7b5b100bba49a727460
Pierce Issah <PJIceskull2003@gmail.com>
2022-09-27 11:28:10 -0400

    created arrays for colors

--- 6_Arrays_and_Loops/inClass/example1/app.js
+++ 6_Arrays_and_Loops/inClass/example1/app.js
@@ -1,7 +1,12 @@
 // Variables
-let colors = ["#042a2b", "red", "purple"];
+let colors = ["#18206f", "#17255A", "#F5E2C8", "#D88373", "#BD1E1E", "red", "purple"];
 
 // for(var i = 0; i < colors.length; i++) {
 for (var i = 0; i < 6; i++) {
     let aDiv = document.createElement("div");
+    aDiv.innerHTML = colors[i];
+    aDiv.style.width = "100px";
+    aDiv.style.height = "100px";
+    aDiv.style.backgroundColor = colors[i];
+    document.body.appendChild(aDiv);
 }
