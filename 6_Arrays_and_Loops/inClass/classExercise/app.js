0363c5879e80fbe9a93a20d0cacc06b3dcff4029
Pierce Issah <PJIceskull2003@gmail.com>
2022-09-28 12:53:34 -0400

    add comment

--- 6_Arrays_and_Loops/inClass/classExercise/app.js
+++ 6_Arrays_and_Loops/inClass/classExercise/app.js
@@ -1,6 +1,7 @@
 // Variable & Arrays
 let words = ["Indiana", "University", "Purdue", "University", "Indianapolis"];
-let wordsReverse = ["Indiana", "University", "Purdue", "University", "Indianapolis"].reverse();
+let wordsReverse = ["Indiana", "University", "Purdue", "University", "Indianapolis"].reverse(); // Reverse the order of the words with reverse()
+
 // Array Index #:    0,  1,  2,  3,  4,  5
 
 // For Loops
@@ -12,13 +13,13 @@ let wordsReverse = ["Indiana", "University", "Purdue", "University", "Indianapol
 // Print Words Loop
 // for (let i = 0; i < words.length; i++) {
 //     //  Print Words in Arrays
-//     // console.log(words[i] + "!" + " Yeah!");
-//     console.log(words[i] + "!" + " " + "Yeah")
+//     console.log(words[i] + "!" + " Yeah!"); // Print "Words" + an ! + "Yeah" to console
+//     // console.log(words[i] + "!" + " " + "Yeah") // different way to print line 16
 // }
 
 // // Captialize Words
 // for (let i = 0; i < words.length; i++) {
-//     console.log(words[i].toUpperCase());
+//     console.log(words[i].toUpperCase()); // Print and make the letters in the words uppercase using
 // }
 
 // Count from 10 to 1
@@ -34,6 +35,6 @@ let wordsReverse = ["Indiana", "University", "Purdue", "University", "Indianapol
 // }
 
 // Captialize Words
-for (let i = 0; i < words.length; i++) {
-    console.log(wordsReverse[i]);
-}
+// for (let i = 0; i < words.length; i++) {
+//     console.log(wordsReverse[i]); // Print the reverse order of the words
+// }
