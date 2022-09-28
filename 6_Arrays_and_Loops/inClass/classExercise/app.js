69fe4e1a911df479cfbaadc208929a55395dc66d
Pierce Issah <PJIceskull2003@gmail.com>
2022-09-28 12:09:49 -0400

    created files for projects

---
+++ 6_Arrays_and_Loops/inClass/classExercise/app.js
@@ -0,0 +1,8 @@
+// Variable & Arrays
+let shoeBands = [];
+// Array Index #:    0,  1,  2,  3,  4,  5
+// For Loops
+for (let i = 0; i < array.length; i++) {
+    // do things
+    // increase i by 1
+}
