00b5b4083c4829f389d77f43e30ab18c75db82af
Pierce Issah <PJIceskull2003@gmail.com>
2022-09-28 12:44:11 -0400

    add loops

--- 6_Arrays_and_Loops/inClass/classExercise/app.js
+++ 6_Arrays_and_Loops/inClass/classExercise/app.js
@@ -1,8 +1,38 @@
 // Variable & Arrays
-let shoeBands = [];
+let words = ["Indiana", "University", "Purdue", "University", "Indianapolis"];
 // Array Index #:    0,  1,  2,  3,  4,  5
+
 // For Loops
-for (let i = 0; i < 10; i++) {
-    // do things
-    // increase i by 1
+// Count to 10 Loop
+// for (let i = 1; i < 11; i++) {
+//     // console.log(i); // console the numbers 1 to 10
+// }
+
+// Print Words Loop
+// for (let i = 0; i < words.length; i++) {
+//     //  Print Words in Arrays
+//     // console.log(words[i] + "!" + " Yeah!");
+//     console.log(words[i] + "!" + " " + "Yeah")
+// }
+
+// // Captialize Words
+// for (let i = 0; i < words.length; i++) {
+//     console.log(words[i].toUpperCase());
+// }
+
+// Count from 10 to 1
+// for (i = 10; i > 0; i--) {
+//     console.log(i);
+//     // i = i - 1;
+// }
+
+// Fibonacci sequence
+// for (i = 0; i < 20; i++) {
+//     let fibonacci = [];
+//     console.log(i)
+// }
+
+// Captialize Words
+for (let i = 0; i < words.length; i++) {
+    console.log(words[i]);
 }
