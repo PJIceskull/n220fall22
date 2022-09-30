fe37f1a776cb0c1a58a603d924374e1404a6ee3f
Pierce Issah <PJIceskull2003@gmail.com>
2022-09-30 19:31:15 -0400

    add comments

--- 5_DomManipulation_and_CSS/DOM_Exercises/McDiv'ns/app.js
+++ 5_DomManipulation_and_CSS/DOM_Exercises/McDiv'ns/app.js
@@ -1,23 +1,24 @@
 // Variables
 let mcDiv = document.getElementById("mcDiv"); // Target mcDiv ID in html file
 var clickCount = 0; // Variable for counting the # of mouse clicks
-let mcText = "mc";
+let mcText = "mc"; // Create Text String for "mc" Text
 
 // Target Div styles 
-mcDiv.style.color = "#ffffff";
-mcDiv.style.backgroundColor = "#0033cc";
+mcDiv.style.color = "#ffffff"; // Set color of text to White
+mcDiv.style.backgroundColor = "#0033cc"; // Set BG color to a blue-ish color
+// Set the Width & height of the Div to 150px
 mcDiv.style.width = "150px";
 mcDiv.style.height = "150px";
-mcDiv.style.margin = "0 auto";
+mcDiv.style.margin = "0 auto"; // Set Div to the center of the screen
 mcDiv.innerHTML = mcText;
 
 // Onclick Function
 function addText() {
     mcText = mcText + "mc"; // Add one "mc" text to mcTExt string 
     mcDiv.innerHTML = mcText;
-    clickCount = clickCount + 1;
-    console.log(clickCount);
-    if (clickCount >= 3) {
+    clickCount = clickCount + 1; // Increase the ClickCount by 1
+    console.log(clickCount); // Print the number of the clickCount to Console
+    if (clickCount >= 3) { // If the clickCount is equal to or greater than 3
         mcDiv.style.backgroundColor = "#ff6600"; // Change background color when click on
         mcDiv.innerHTML = "divdivdiv";
     }
