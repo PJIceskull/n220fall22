1e4e5d9d30ca84f20c4143463ed168efc14ef831
Pierce Issah <PJIceskull2003@gmail.com>
2022-09-29 22:41:26 -0400

    target styles in javascript

--- 5_DomManipulation_and_CSS/DOM_Exercises/Peak_Pixels/app.js
+++ 5_DomManipulation_and_CSS/DOM_Exercises/Peak_Pixels/app.js
@@ -1,2 +1,8 @@
 // Variables 
 let squareDiv = document.getElementById("squareDiv");
+
+// Square Div styles
+squareDiv.style.width = "100px";
+squareDiv.style.height = "100px";
+squareDiv.style.color = "#FFFFFF";
+squareDiv.style.backgroundColor = "#049a04";
