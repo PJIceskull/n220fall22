390ed12ccc2dc67cb08bc2bc7264d1e01cf91e81
Pierce Issah <PJIceskull2003@gmail.com>
2022-09-30 14:50:40 -0400

    created a new variable and styles

--- 5_DomManipulation_and_CSS/DOM_Exercises/Peak_Pixels/app.js
+++ 5_DomManipulation_and_CSS/DOM_Exercises/Peak_Pixels/app.js
@@ -1,6 +1,7 @@
 // Variables 
 let squareDiv = document.getElementById("squareDiv");
 let divHeight = 100; // height of the div is 100 px
+increaseSize = 1.5; // Variable that increase values by 10%
 
 // Square Div styles
 squareDiv.style.width = divHeight + "px";
@@ -8,10 +9,11 @@ squareDiv.style.height = divHeight + "px";
 squareDiv.style.color = "#FFFFFF";
 squareDiv.style.backgroundColor = "#049a04";
 squareDiv.style.margin = "0 auto";
+squareDiv.style.textAlign = "center";
 
 function enLarge() {
     // squareDiv.style.backgroundColor = "#000"; // Change Background Color to test Function
-    squareDiv.style.height = divHeight * 1.1 + "px";
-    squareDiv.style.width = divHeight * 1.1 + "px";
+    squareDiv.style.height = divHeight * increaseSize + "px";
+    squareDiv.style.width = divHeight * increaseSize + "px";
     // divHeight * 1.1 + "px";
 }
