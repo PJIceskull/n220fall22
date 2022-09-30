b13562521bed51315ba95328b6c063b5905ec5a8
Pierce Issah <PJIceskull2003@gmail.com>
2022-09-29 22:43:13 -0400

    targert more styles

--- 5_DomManipulation_and_CSS/DOM_Exercises/Peak_Pixels/app.js
+++ 5_DomManipulation_and_CSS/DOM_Exercises/Peak_Pixels/app.js
@@ -6,3 +6,4 @@ squareDiv.style.width = "100px";
 squareDiv.style.height = "100px";
 squareDiv.style.color = "#FFFFFF";
 squareDiv.style.backgroundColor = "#049a04";
+squareDiv.style.margin = "0 auto";
