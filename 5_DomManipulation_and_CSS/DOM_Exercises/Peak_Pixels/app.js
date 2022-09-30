3db53a64f92848715961dad173018f6d4a2f254f
Pierce Issah <PJIceskull2003@gmail.com>
2022-09-30 14:58:05 -0400

    increase divSize when it's click on

--- 5_DomManipulation_and_CSS/DOM_Exercises/Peak_Pixels/app.js
+++ 5_DomManipulation_and_CSS/DOM_Exercises/Peak_Pixels/app.js
@@ -1,7 +1,7 @@
 // Variables 
 let squareDiv = document.getElementById("squareDiv");
 let divHeight = 100; // height of the div is 100 px
-increaseSize = 1.5; // Variable that increase values by 10%
+var increaseSize = 1.1; // Variable that increase values by 10%
 
 // Square Div styles
 squareDiv.style.width = divHeight + "px";
@@ -13,7 +13,10 @@ squareDiv.style.textAlign = "center";
 
 function enLarge() {
     // squareDiv.style.backgroundColor = "#000"; // Change Background Color to test Function
-    squareDiv.style.height = divHeight * increaseSize + "px";
-    squareDiv.style.width = divHeight * increaseSize + "px";
+    divHeight = divHeight * increaseSize; // New div size value is the divHeight time increaseSize, which is 100 x 1.1
+    squareDiv.style.height = divHeight + "px";
+    squareDiv.style.width = divHeight + "px";
+    // squareDiv.style.height = divHeight * increaseSize + "px";
+    // squareDiv.style.width = divHeight * increaseSize + "px";
     // divHeight * 1.1 + "px";
 }
