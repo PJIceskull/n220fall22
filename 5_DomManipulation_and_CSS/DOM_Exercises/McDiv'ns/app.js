bfe06d68ddb4e71dbd747c14e02ef61746573840
Pierce Issah <PJIceskull2003@gmail.com>
2022-09-30 19:06:38 -0400

    add code to addText Function

--- 5_DomManipulation_and_CSS/DOM_Exercises/McDiv'ns/app.js
+++ 5_DomManipulation_and_CSS/DOM_Exercises/McDiv'ns/app.js
@@ -1,16 +1,26 @@
 // Variables
 let mcDiv = document.getElementById("mcDiv"); // Target mcDiv ID in html file
 var clickCount = 0; // Variable for counting the # of mouse clicks
+let mcText = "mc";
 
 // Target Div styles 
 mcDiv.style.color = "#ffffff";
 mcDiv.style.backgroundColor = "#0033cc";
-mcDiv.style.width = "100px";
-mcDiv.style.height = "100px";
+mcDiv.style.width = "150px";
+mcDiv.style.height = "150px";
 mcDiv.style.margin = "0 auto";
+mcDiv.innerHTML = mcText;
 
 // Onclick Function
 function addText() {
-    mcDiv.style.backgroundColor = "#ff6600"; // Change background color when click on
-    mcDiv.innerHTML = "mc";
+    mcText = mcText + "mc"; // Add one "mc" text to mcTExt string 
+    mcDiv.innerHTML = mcText;
+    clickCount = clickCount + 1;
+    console.log(clickCount);
+    if (clickCount >= 3) {
+        mcDiv.style.backgroundColor = "#ff6600"; // Change background color when click on
+        mcDiv.innerHTML = "divdivdiv";
+    }
+    // mcDiv.innerHTML = "mc" + "mc";
+    // mcDiv.appendChild()
 }
