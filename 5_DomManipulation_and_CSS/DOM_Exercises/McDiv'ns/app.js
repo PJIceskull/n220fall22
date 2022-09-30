11906f8857686c72e3e0fa02f06ada40de8e78b3
Pierce Issah <PJIceskull2003@gmail.com>
2022-09-30 16:47:13 -0400

    target styles in mcDiv div

--- 5_DomManipulation_and_CSS/DOM_Exercises/McDiv'ns/app.js
+++ 5_DomManipulation_and_CSS/DOM_Exercises/McDiv'ns/app.js
@@ -1,2 +1,11 @@
 // Variables
 let mcDiv = document.getElementById("mcDiv"); // Target mcDiv ID in html file
+
+// Target Div styles 
+mcDiv.style.color = "#ffffff";
+mcDiv.style.backgroundColor = "#0033cc";
+mcDiv.style.width = "100px";
+mcDiv.style.height = "100px";
+mcDiv.style.margin = "0 auto";
+
+// Onclick Function
