9fd69c32c2974a915e564eb0bcce3c9fb45d78af
Pierce Issah <PJIceskull2003@gmail.com>
2022-09-29 22:45:08 -0400

    created Div Height Variable

--- 5_DomManipulation_and_CSS/DOM_Exercises/Peak_Pixels/app.js
+++ 5_DomManipulation_and_CSS/DOM_Exercises/Peak_Pixels/app.js
@@ -1,9 +1,14 @@
 // Variables 
 let squareDiv = document.getElementById("squareDiv");
+let divHeight = "100px"; // height of the div is 100 px
 
 // Square Div styles
-squareDiv.style.width = "100px";
-squareDiv.style.height = "100px";
+squareDiv.style.width = divHeight;
+squareDiv.style.height = divHeight;
 squareDiv.style.color = "#FFFFFF";
 squareDiv.style.backgroundColor = "#049a04";
 squareDiv.style.margin = "0 auto";
+
+function enLarge() {
+
+}
