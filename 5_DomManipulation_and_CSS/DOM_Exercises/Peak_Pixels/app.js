54c144fefb611d182b4fecf673bf943cc2e28706
Pierce Issah <PJIceskull2003@gmail.com>
2022-09-29 23:02:01 -0400

    code to increase div size by 1.1

--- 5_DomManipulation_and_CSS/DOM_Exercises/Peak_Pixels/app.js
+++ 5_DomManipulation_and_CSS/DOM_Exercises/Peak_Pixels/app.js
@@ -1,14 +1,17 @@
 // Variables 
 let squareDiv = document.getElementById("squareDiv");
-let divHeight = "100px"; // height of the div is 100 px
+let divHeight = 100; // height of the div is 100 px
 
 // Square Div styles
-squareDiv.style.width = divHeight;
-squareDiv.style.height = divHeight;
+squareDiv.style.width = divHeight + "px";
+squareDiv.style.height = divHeight + "px";
 squareDiv.style.color = "#FFFFFF";
 squareDiv.style.backgroundColor = "#049a04";
 squareDiv.style.margin = "0 auto";
 
 function enLarge() {
-
+    // squareDiv.style.backgroundColor = "#000"; // Change Background Color to test Function
+    squareDiv.style.height = divHeight * 1.1 + "px";
+    squareDiv.style.width = divHeight * 1.1 + "px";
+    // divHeight * 1.1 + "px";
 }
