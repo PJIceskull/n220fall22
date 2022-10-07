a8afb06c9619b4bf0b37ddcd12310950a601a9f8
Pierce Issah <PJIceskull2003@gmail.com>
2022-10-07 16:28:45 -0400

    print out count for iterand var

--- 6_Arrays_and_Loops/loopsArraysExercises/BeepBop/app.js
+++ 6_Arrays_and_Loops/loopsArraysExercises/BeepBop/app.js
@@ -3,6 +3,8 @@ let divLoop = document.getElementById("divLoop"); // Target the div with the id 
 let iterand = 0; // Variable to count the number iterations in our Loop
 
 // Loop
-for (var i = 0; i < 25; i++) { // Use i value to count loop
-    console.log(i);
+for (var i = 0; i < 26; i++) { // Use i value to count loop
+    iterand = i;
+    // console.log(i); // use console log to count the number of times that the lop runs
+    console.log(iterand); // Print the number of times that iterand rans
 }
