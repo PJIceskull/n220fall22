a5baea23797fe8a367718f92ae09704ffc8bd19a
Pierce Issah <PJIceskull2003@gmail.com>
2022-10-07 16:25:47 -0400

    created loop function

--- 6_Arrays_and_Loops/loopsArraysExercises/BeepBop/app.js
+++ 6_Arrays_and_Loops/loopsArraysExercises/BeepBop/app.js
@@ -1,3 +1,8 @@
 // Variables
 let divLoop = document.getElementById("divLoop"); // Target the div with the id named "divLoop"
 let iterand = 0; // Variable to count the number iterations in our Loop
+
+// Loop
+for (var i = 0; i < 25; i++) { // Use i value to count loop
+    console.log(i);
+}
