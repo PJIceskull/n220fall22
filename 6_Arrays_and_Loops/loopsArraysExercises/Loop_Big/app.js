cdbc526769b825d6fd08222e0f10a87b65772a6d
Pierce Issah <PJIceskull2003@gmail.com>
2022-10-07 15:45:29 -0400

    use var to decribe variables

--- 6_Arrays_and_Loops/loopsArraysExercises/Loop_Big/app.js
+++ 6_Arrays_and_Loops/loopsArraysExercises/Loop_Big/app.js
@@ -3,7 +3,7 @@ let thousands = [1000, 2000, 3000, 4000, 5000]; // Named Array thousands since a
 let loopDiv = document.getElementById("loopDiv"); // Target "loopDiv" id in HTML file
 
 // for loop
-for (let i = 0; i < thousands.length; i++) { // Use i as a counter and stop at the length of array
+for (var i = 0; i < thousands.length; i++) { // Use i as a counter and stop at the length of array
     // console.log(thousands[i]);
     loopDiv.innerHTML = thousands; // Display Numbers in Thousands Array
 }
