9ca8cab3e6d7ad803ae765542378c397a9de15e4
Pierce Issah <PJIceskull2003@gmail.com>
2022-10-07 12:02:12 -0400

    created loopDiv Variable

--- 6_Arrays_and_Loops/loopsArraysExercises/Loop_Big/app.js
+++ 6_Arrays_and_Loops/loopsArraysExercises/Loop_Big/app.js
@@ -1,2 +1,3 @@
 // Variables 
 let thousands = [1000, 2000, 3000, 4000, 5000]; // Named Array thousands since all values are in the thousands 
+let loopDiv = document.getElementById("loopDiv"); // Target "loopDiv" id in HTML file
