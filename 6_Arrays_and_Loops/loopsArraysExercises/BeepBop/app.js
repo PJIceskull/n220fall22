844dd6c15fdaf47e166bcd356c49b1744480b001
Pierce Issah <PJIceskull2003@gmail.com>
2022-10-13 11:27:18 -0400

    Made Changes to code

--- 6_Arrays_and_Loops/loopsArraysExercises/BeepBop/app.js
+++ 6_Arrays_and_Loops/loopsArraysExercises/BeepBop/app.js
@@ -1,10 +1,17 @@
 // Variables
 let divLoop = document.getElementById("divLoop"); // Target the div with the id named "divLoop"
 let iterand = 0; // Variable to count the number iterations in our Loop
+text = "Count is";
+
 
 // Loop
-for (var i = 0; i < 26; i++) { // Use i value to count loop
-    iterand = i;
+for (var i = 0; i <= 25; i++) { // Use i value to count loop
+    iterand = "" + i;
     // console.log(i); // use console log to count the number of times that the lop runs
     console.log(iterand); // Print the number of times that iterand rans
+    divLoop.innerHTML = iterand;
+    // if (iterand % 3 == 0) {
+    //     console.log("Beep");
+    //     divLoop.innerText = "Beep";
+    // }
 }
