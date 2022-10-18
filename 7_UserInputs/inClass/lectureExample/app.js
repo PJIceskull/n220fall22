7107e7a290425e24b34dc920573d6d84f96c15d1
Pierce Issah <PJIceskull2003@gmail.com>
2022-10-17 23:02:45 -0400

    commented out code

--- 7_UserInputs/inClass/lectureExample/app.js
+++ 7_UserInputs/inClass/lectureExample/app.js
@@ -1,2 +1,5 @@
 let txtName = document.getElementById("txtName"); // Create txtname variable by target id
-console.log(txtName); // print txtName to console
+// console.log(txtName); // print txtName to console
+
+// rollDice Function
+// function
