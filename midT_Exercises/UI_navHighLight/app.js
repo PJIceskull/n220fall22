46385fd3877a0737b7ac74fc7869d4e37296154b
Pierce Issah <PJIceskull2003@gmail.com>
2022-10-18 16:21:50 -0400

    created fruits array

--- midT_Exercises/UI_navHighLight/app.js
+++ midT_Exercises/UI_navHighLight/app.js
@@ -1,2 +1,8 @@
 // Variables
-let fruitDiv = document.getElementById("fruitDiv"); // Target "fruitDiv" id in divs
+let fruits = ["Apple", "Orange", "Bannana", "Grape", "Strawberry", "PineApple"] // An arrays for fruit names
+// let fruitDiv = document.getElementById("fruitDiv"); // Target "fruitDiv" id in divs
+let fruitDiv = document.getElementsByName(fruits); //
+
+// fruitDiv.style.height = 150 + "px";
+// fruitDiv.style.width = 150 + "px";
+// fruitDiv.style.backgroundColor = "gold";
