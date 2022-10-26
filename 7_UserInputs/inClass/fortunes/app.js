f15073cac82ff5d8d1aa73c28ce0b555bd31a9b3
Pierce Issah <PJIceskull2003@gmail.com>
2022-10-26 13:03:29 -0400

    added comments

--- 7_UserInputs/inClass/fortunes/app.js
+++ 7_UserInputs/inClass/fortunes/app.js
@@ -1,13 +1,12 @@
 // Variables
 fortunes = ["Your time to now!", "You will die eventually.", "You will be succesfully in life."]; // Arrays for fortunes
 newFortune = document.getElementById("newFortune"); // Target Div with the ID "newFortune"
-// Vars for random values
-
 
 function getFortune() {
     console.log("Clicked"); // Print to console when button is clicked
     // Put random variable inside function
-    let randIndex = Math.floor(Math.random() * fortunes.length); //
-    let randEntry = fortunes[randIndex]; //
+    let randIndex = Math.floor(Math.random() * fortunes.length); // Generate Random Value with the length of our array
+    let randEntry = fortunes[randIndex]; // Assign the random index of our Fortune Arrays to randEntry Var
+
     newFortune.innerHTML = randEntry; // Assign the radnom generated fortune entry into the div. 
 }
