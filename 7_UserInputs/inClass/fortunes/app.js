a227ebbadfc6e46be330b9c193fae2ca8a7b7ae0
Pierce Issah <PJIceskull2003@gmail.com>
2022-10-26 12:40:06 -0400

    created vars for random values

--- 7_UserInputs/inClass/fortunes/app.js
+++ 7_UserInputs/inClass/fortunes/app.js
@@ -1,7 +1,9 @@
 // Variables
 fortunes = ["Your time to now!", "You will die eventually.", "You will be succesfully in life."]; // Arrays for fortunes
 newFortune = document.getElementById("newFortune"); // Target Div with the ID "newFortune"
-
+// Vars for random values
+let randIndex = Math.random() * fortunes.length;
+let randEntry = fortunes[randIndex];
 
 function getFortune() {
     console.log("CLicked") // PRint to console when button is clicked
