3db9a8d2731c235234c199c75cfaca75a6e1da41
Pierce Issah <PJIceskull2003@gmail.com>
2022-10-27 19:57:19 -0400

    add code to printTExt

--- 7_UserInputs/userInput_Exercise/Greeter/app.js
+++ 7_UserInputs/userInput_Exercise/Greeter/app.js
@@ -1,7 +1,9 @@
-// Variables
-userValue = document.getElementById("userValue"); // Target tag with the ID: "userValue"
+// Variable
+let userValue = document.getElementById("userValue"); // Target tag with the ID: "userValue"
 
 
 function printText() {
+    let userText = userValue.innerHTML;
 
+    console.log("Hello " + userText);
 }
