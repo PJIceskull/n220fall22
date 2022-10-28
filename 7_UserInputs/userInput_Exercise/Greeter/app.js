c09d592fb87bed481a496523547af4f9fdcacbbc
Pierce Issah <PJIceskull2003@gmail.com>
2022-10-27 22:15:59 -0400

    created greetTExt variable

--- 7_UserInputs/userInput_Exercise/Greeter/app.js
+++ 7_UserInputs/userInput_Exercise/Greeter/app.js
@@ -1,11 +1,14 @@
 // Variable
 let userValue = document.getElementById("userValue"); // Target tag with the ID: "userValue"
-console.log(userValue.value); // Print to Console the current value
+let greetText = document.getElementById("greetText");
 
+console.log(userValue.value); // Print to Console the current value
 
 function printText() {
     // let userText = userValue.innerHTML;
     let userText = userValue.value; // Assign the value the User typed to var UserText
 
     console.log("Hello" + " " + userText); // Print to Console "Hello + "User Value" "
+
+    greetText.innerHTML = "Hello " + userText;
 }
