e5a4605ce6c6f3253ffbf474114c559b8b390035
Pierce Issah <PJIceskull2003@gmail.com>
2022-10-27 21:52:17 -0400

    create textInsert id and verifyText Array

--- 7_UserInputs/userInput_Exercise/numberGuesser/app.js
+++ 7_UserInputs/userInput_Exercise/numberGuesser/app.js
@@ -1,5 +1,7 @@
 // Variables
-userGuess = document.getElementById("userGuess"); // Target the element with the ID, userGuess 
+let userGuess = document.getElementById("userGuess"); // Target the element with the ID, userGuess 
+let textInsert = document.getElementById("textInsert"); // Target element with ID, textInsert
+let verifyText = ["Way Too High! Try Guessing lower", "Too High", "BULLSEYE!!", "Way Too Low! Try guessing lower", "Too Low!"];
 
 
 // Function to check the user's guess
