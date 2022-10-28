a71d43ff6658e53c29c97c5accb227b75a3c061f
Pierce Issah <PJIceskull2003@gmail.com>
2022-10-27 22:06:23 -0400

    Write comments

--- 7_UserInputs/userInput_Exercise/numberGuesser/app.js
+++ 7_UserInputs/userInput_Exercise/numberGuesser/app.js
@@ -1,7 +1,7 @@
 // Variables
 let userGuess = document.getElementById("userGuess"); // Target the element with the ID, userGuess 
 let textInsert = document.getElementById("textInsert"); // Target element with ID, textInsert
-let verifyText = ["Way Too High! Try Guessing lower", "BULLSEYE!!", "Way Too Low! Try guessing Higher"];
+let verifyText = ["Way Too High! Try Guessing lower", "BULLSEYE!!", "Way Too Low! Try guessing Higher"]; // Text placeholders for the user's answer
 
 // Random generated number between 1 and 20 
 let guessNumber = Math.floor(Math.random() * 20) + 1;
@@ -9,15 +9,15 @@ console.log(guessNumber); // Print to console random number
 
 // Function to check the user's guess
 function checkGuess() {
-    if (userGuess.value == guessNumber) {
-        textInsert.innerHTML = verifyText[1];
-        textInsert.style.color = "#009933";
-    } else if (userGuess.value > guessNumber) {
-        textInsert.innerHTML = verifyText[0];
-        textInsert.style.color = "#000000";
-    } else {
-        textInsert.innerHTML = verifyText[2];
-        textInsert.style.color = "#000000";
+    if (userGuess.value == guessNumber) { // if user's guess is the random number
+        textInsert.innerHTML = verifyText[1]; // Say Bullseye!
+        textInsert.style.color = "#009933"; // set font color to green
+    } else if (userGuess.value > guessNumber) { // if user guess if higher than random number
+        textInsert.innerHTML = verifyText[0]; // Say user was too high
+        textInsert.style.color = "#000000"; // set font color to black
+    } else { // if user guess is less than random number
+        textInsert.innerHTML = verifyText[2]; // say user was too loe
+        textInsert.style.color = "#000000"; // set font color to black
     }
 
 }
