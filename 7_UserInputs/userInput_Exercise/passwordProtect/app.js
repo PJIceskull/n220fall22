3adafc00ed90fed91a661bf9dd7460134cb81cbf
Pierce Issah <PJIceskull2003@gmail.com>
2022-10-27 20:49:09 -0400

    print values to console

--- 7_UserInputs/userInput_Exercise/passwordProtect/app.js
+++ 7_UserInputs/userInput_Exercise/passwordProtect/app.js
@@ -2,6 +2,12 @@
 userName = document.getElementById("userName"); // Target element with the Id: userName
 passWord = document.getElementById("passWord"); // Target element with the Id: passWord
 
-function checkdata() {
+// Print out Current Values of Username and Pssword
+console.log(userName.value);
+console.log(passWord.value);
+
+function checkData() {
+    // Test Function
+    console.log(userName + " " + passWord);
 
 }
