81bbba1c0401634d985f5f885fcf8cd2558b3fef
Pierce Issah <PJIceskull2003@gmail.com>
2022-10-27 21:55:36 -0400

    create rand Generate number

--- 7_UserInputs/userInput_Exercise/numberGuesser/app.js
+++ 7_UserInputs/userInput_Exercise/numberGuesser/app.js
@@ -3,6 +3,9 @@ let userGuess = document.getElementById("userGuess"); // Target the element with
 let textInsert = document.getElementById("textInsert"); // Target element with ID, textInsert
 let verifyText = ["Way Too High! Try Guessing lower", "Too High", "BULLSEYE!!", "Way Too Low! Try guessing lower", "Too Low!"];
 
+// Random generated number between 1 and 21 
+let guessNumber = Math.floor(Math.random() * 21) + 1;
+console.log(guessNumber);
 
 // Function to check the user's guess
 function checkGuess() { }
