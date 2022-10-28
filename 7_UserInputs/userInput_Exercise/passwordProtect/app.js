c325fbf9ef27e75884dbc8b433e97dcf49c733e3
Pierce Issah <PJIceskull2003@gmail.com>
2022-10-27 20:57:28 -0400

    created new var

--- 7_UserInputs/userInput_Exercise/passwordProtect/app.js
+++ 7_UserInputs/userInput_Exercise/passwordProtect/app.js
@@ -1,6 +1,8 @@
 // Variable
 userName = document.getElementById("userName"); // Target element with the Id: userName
 passWord = document.getElementById("passWord"); // Target element with the Id: passWord
+verifyInfo = document.getElementById("verifyInfo");
+// Target Element with ID, verifyInfo
 
 // Print out Current Values of Username and Pssword
 console.log(userName.value);
@@ -9,5 +11,7 @@ console.log(passWord.value);
 function checkData() {
     // Test Function
     console.log(userName + " " + passWord);
-
+    if (userName.value == "Username") {
+        console.log("Correct");
+    }
 }
