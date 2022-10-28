0a506a315ecf3486114577e717c537043a346f3b
Pierce Issah <PJIceskull2003@gmail.com>
2022-10-27 20:05:27 -0400

    commented and assign value to userText

--- 7_UserInputs/userInput_Exercise/Greeter/app.js
+++ 7_UserInputs/userInput_Exercise/Greeter/app.js
@@ -1,9 +1,11 @@
 // Variable
 let userValue = document.getElementById("userValue"); // Target tag with the ID: "userValue"
+console.log(userValue.value); // Print to Console the current value
 
 
 function printText() {
-    let userText = userValue.innerHTML;
+    // let userText = userValue.innerHTML;
+    let userText = userValue.value; // Assign the value the User typed to var UserText
 
-    console.log("Hello " + userText);
+    console.log("Hello" + "" + userText); // Print to Console "Hello + "User Value" "
 }
