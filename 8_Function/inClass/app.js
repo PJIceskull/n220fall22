1aded5c60a77ed69f6efc63ebd6d843b1ce6d3df
Pierce Issah <PJIceskull2003@gmail.com>
2022-11-02 14:33:51 -0400

    created isVowel function

--- 8_Function/inClass/app.js
+++ 8_Function/inClass/app.js
@@ -1,10 +1,37 @@
 // Variables
 let vowels = ["a", "e", "i", "o", "u"];
-let userInput = document.getElementById("userInput");
+let userInput = document.getElementById("userInput"); // Target ID with "userInput"
+let testString = "Spoookoy";
+let result = removeVowel(testString);
+
+
+console.log(vowels);
+console.log(result);
 
 // Function One - Identify Vowel
-function checkLetter() {
-    console.log(userInput.value);
+function isVowel(char) {
+    let letter = char.toLocaleLowerCase();
+    for (i = 0; i < vowels.length; i++) {
+        if (letter == vowels[i]) {
+            return true;
+        }
+    }
+    return false;
 }
+
+// function checkLetter() {
+//     let inputWord = userInput.value; // Local Variable to hold user input value
+//     console.log(inputWord); // Print to console the user's input
+//     for (i = 0; i > userInput.value; i++) { }
+// }
+
 // Function Two - Remove Vowels
-function removeVowel() { }
+function removeVowel(inputStr) {
+    let returnStr = "";
+    for (let i = 0; i < inputStr.length; i++) {
+        if (!isVowel(inputStr[i])) {
+            returnStr += inputStr[i];
+        }
+    }
+    return returnStr;
+}
