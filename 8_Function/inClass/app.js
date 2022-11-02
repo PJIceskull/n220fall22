51f0bc6741b98b36be81afd670ddd61dfff2c172
Pierce Issah <PJIceskull2003@gmail.com>
2022-11-02 12:17:47 -0400

    created var & functions

--- 8_Function/inClass/app.js
+++ 8_Function/inClass/app.js
@@ -1,6 +1,7 @@
 // Variables
-
+let vowels = ["a", "e", "i", "o", "u"];
 
 // Function One - Identify Vowel
-
+function checkLetter() { }
 // Function Two - Remove Vowels
+function removeVowel() { }
