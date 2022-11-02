43d1d9f16bacfbbac4b13ffe5a0fe68eff86cc36
Pierce Issah <PJIceskull2003@gmail.com>
2022-11-02 12:23:40 -0400

    use console to test function

--- 8_Function/inClass/app.js
+++ 8_Function/inClass/app.js
@@ -2,8 +2,9 @@
 let vowels = ["a", "e", "i", "o", "u"];
 let userInput = document.getElementById("userInput");
 
-
 // Function One - Identify Vowel
-function checkLetter() { }
+function checkLetter() {
+    console.log(userInput.value);
+}
 // Function Two - Remove Vowels
 function removeVowel() { }
