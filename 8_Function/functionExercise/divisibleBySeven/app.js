8103985be96e21f74bcefe15fc3f2559bd4a3225
Pierce Issah <PJIceskull2003@gmail.com>
2022-11-03 19:41:13 -0400

    write comments for if statment

--- 8_Function/functionExercise/divisibleBySeven/app.js
+++ 8_Function/functionExercise/divisibleBySeven/app.js
@@ -7,6 +7,10 @@ console.log(userInput); // Print to the console the userInput
 // Function to check the number that user entered
 function checkNum() {
     console.log(userInput.value); // PRint to console the user's input
+    isDivisible(7); // find if value is divisibe by 7
+    if (userInput.value == true) {
+        textResult.innerText = "This number can be divide by 7!";
+    }
 }
 
 // Function is see if number is divisible by Number/7
@@ -14,8 +18,8 @@ function isDivisible(num) { // "num" stands for number
     // "quotient" is the solution in dividing numbers
     quotient = userInput.value % num; // The user's value divided by our number
     console.log(quotient); // Print to console the "Quotient"
-    if (quotient == 0) {
-        return true;
+    if (quotient == 0) { // If the remainder is "0"
+        return true; // return "true"
     }
-    return false;
+    return false; // else, return "False"
 }
