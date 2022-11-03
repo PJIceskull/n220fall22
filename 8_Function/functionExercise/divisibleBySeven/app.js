e43b7fb3f7803ad8dbb9979aaefcf3d59b81fd4d
Pierce Issah <PJIceskull2003@gmail.com>
2022-11-03 19:15:07 -0400

    create CheckNum function

--- 8_Function/functionExercise/divisibleBySeven/app.js
+++ 8_Function/functionExercise/divisibleBySeven/app.js
@@ -1,2 +1,8 @@
 // Variables
 let userInput = document.getElementById("userInput"); // Target Element with ID, userInput
+
+console.log(userInput); // Print to the console the userInput
+
+function checkNum() {
+    console.log(userInput.value);
+}
