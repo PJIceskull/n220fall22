ad645ec85170ea488d1bd4c208c61017313f2713
Pierce Issah <PJIceskull2003@gmail.com>
2022-11-03 20:35:36 -0400

    assign random value to resultNum

--- 8_Function/functionExercise/makeRandom/app.js
+++ 8_Function/functionExercise/makeRandom/app.js
@@ -1,17 +1,18 @@
 // Variables
-displayResult = document.getElementById("displayResult"); // targert element with ID, display result;
-resultNum = 0 // Have inital value be 0
+let displayResult = document.getElementById("displayResult"); // targert element with ID, display result;
+let resultNum = 0; // Have inital value be 0
 
 // function for the user click generate button
 function displayNum() {
     console.log("click");
     generateRandom(1, 10); // Generate a number between 1 and 10
     console.log(resultNum); // Print to console the result Number
+    // displayResult
 }
 
 // Order of Parameters in function is minimum Number to Max Number
 function generateRandom(minNum, maxNum) {
     // Return a number from the assign lowest values and highest
-    Math.floor(Math.random() * maxNum) + minNum;
-    return resultNum; // Assign new Random number to result Variable 
+    resultNum = Math.floor(Math.random() * maxNum) + minNum; // Assign new Random number to result Variable 
+    return resultNum;
 }
