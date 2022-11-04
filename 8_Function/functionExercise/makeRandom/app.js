497a0a23828099383439d65ca8e6370d0fdb7a9a
Pierce Issah <PJIceskull2003@gmail.com>
2022-11-03 20:17:17 -0400

    created onclick function

---
+++ 8_Function/functionExercise/makeRandom/app.js
@@ -0,0 +1,10 @@
+// Variables
+displayResult = document.getElementById("displayResult"); // targert element with ID, display result;
+resultNum = 0 // Have inital value be 0
+
+// function for the user click generate button
+function displayNum() {
+    console.log("click");
+}
+
+function generateRandom(num1, num2) { }
