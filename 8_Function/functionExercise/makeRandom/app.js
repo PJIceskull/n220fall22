abd5cea9176cb080452e564fbe19a111bcd58552
Pierce Issah <PJIceskull2003@gmail.com>
2022-11-03 20:30:17 -0400

    using math.random to make random #

--- 8_Function/functionExercise/makeRandom/app.js
+++ 8_Function/functionExercise/makeRandom/app.js
@@ -5,6 +5,13 @@ resultNum = 0 // Have inital value be 0
 // function for the user click generate button
 function displayNum() {
     console.log("click");
+    generateRandom(1, 10); // Generate a number between 1 and 10
+    console.log(resultNum); // Print to console the result Number
 }
 
-function generateRandom(minNum, maxNum) { }
+// Order of Parameters in function is minimum Number to Max Number
+function generateRandom(minNum, maxNum) {
+    // Return a number from the assign lowest values and highest
+    Math.floor(Math.random() * maxNum) + minNum;
+    return resultNum; // Assign new Random number to result Variable 
+}
