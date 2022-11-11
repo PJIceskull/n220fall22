e74432cad04f849c2857acd5bc27c8844a25a2db
Pierce Issah <PJIceskull2003@gmail.com>
2022-11-10 20:59:03 -0500

    created variables

--- 9_ArrayComputation/ArrayComputation_Exercises/makeDivs/app.js
+++ 9_ArrayComputation/ArrayComputation_Exercises/makeDivs/app.js
@@ -3,4 +3,8 @@ let objects = [
     { color: "#FF0000", height: 100, width: 300 },
     { color: "#FFFF00", height: 200, width: 200 },
     { color: "#ff0000", height: 300, width: 100 }
-]
+];
+
+let divContainer = document.getElementById("divContainer"); // Targert Element with Id, "DivContainer"
+
+console.log(divContainer); // Print to console the div tag
