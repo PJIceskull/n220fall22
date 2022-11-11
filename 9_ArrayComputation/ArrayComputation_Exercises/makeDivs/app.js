dd4b291bbd1778e530adb46e69b8f34d94f74b0f
Pierce Issah <PJIceskull2003@gmail.com>
2022-11-10 21:05:27 -0500

    created for loop and makeDiv var

--- 9_ArrayComputation/ArrayComputation_Exercises/makeDivs/app.js
+++ 9_ArrayComputation/ArrayComputation_Exercises/makeDivs/app.js
@@ -4,7 +4,14 @@ let objects = [
     { color: "#FFFF00", height: 200, width: 200 },
     { color: "#ff0000", height: 300, width: 100 }
 ];
-
 let divContainer = document.getElementById("divContainer"); // Targert Element with Id, "DivContainer"
+let makeDiv = document.createElement("div"); // Variable to create div tag.
+
 
 console.log(divContainer); // Print to console the div tag
+
+// For loop
+for (i = 0; i < objects.length; i++) {
+    console.log(objects[i]); // Print to Console the Array index in Objects
+    // Create three divs inside of divContainer
+}
