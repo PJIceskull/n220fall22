fdf2ed6e5084106b4ba95f1c25b22a8f29e0dda5
Pierce Issah <PJIceskull2003@gmail.com>
2022-11-15 11:47:08 -0500

    add margins to wrapper

--- 10_HTMLEvents/HTMLEvent_Exercises/colorChanger/app.js
+++ 10_HTMLEvents/HTMLEvent_Exercises/colorChanger/app.js
@@ -1,9 +1,15 @@
 // Variables
 let divSize = 200; // Number to use for the size values of the divs
-let greyDiv = document.getElementById("greyDiv");
+// let greyDiv = document.getElementById("greyDiv");
+let greyDiv = document.getElementsByClassName("greyDiv"); // Target Elements with the class, greyDiv
 
 // greyDiv.style.width = divSize + "px";
 // greyDiv.style.height = divSize + "px";
 // greyDiv.style.backgroundColor = "#c0c0c0";
 
-// greyDiv.
+// Use Loop write styles for Divs
+for (i = 0; i < greyDiv.length; i++) {
+    greyDiv[i].style.backgroundColor = "#c0c0c0"; //
+    greyDiv[i].style.width = divSize + "px";
+    greyDiv[i].style.height = divSize + "px";
+}
