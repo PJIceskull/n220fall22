0bf17309723dafa5eab04f3c49f1690750635e44
Pierce Issah <PJIceskull2003@gmail.com>
2022-11-15 11:05:36 -0500

    created greyDiv class and target it

---
+++ 10_HTMLEvents/HTMLEvent_Exercises/colorChanger/app.js
@@ -0,0 +1,6 @@
+// Variables
+let divSize = 200; // Number to use for the size values of the divs
+let greyDiv = document.getElementsByClassName("greyDiv"); // Target Element with the Class, "greyDiv"
+
+greyDiv.style.height = divSize + "px";
+greyDiv.style.width = divSize + "px";
