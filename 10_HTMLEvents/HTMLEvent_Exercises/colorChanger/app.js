595e1bf6ea9e5e923faf23f66eb84a3affea6fa7
Pierce Issah <PJIceskull2003@gmail.com>
2022-11-15 11:26:56 -0500

    created greyDiv

--- 10_HTMLEvents/HTMLEvent_Exercises/colorChanger/app.js
+++ 10_HTMLEvents/HTMLEvent_Exercises/colorChanger/app.js
@@ -1,6 +1,9 @@
 // Variables
 let divSize = 200; // Number to use for the size values of the divs
-let greyDiv = document.getElementsByClassName("greyDiv"); // Target Element with the Class, "greyDiv"
+let greyDiv = document.getElementById("greyDiv");
 
-greyDiv.style.height = divSize + "px";
-greyDiv.style.width = divSize + "px";
+// greyDiv.style.width = divSize + "px";
+// greyDiv.style.height = divSize + "px";
+// greyDiv.style.backgroundColor = "#c0c0c0";
+
+// greyDiv.
