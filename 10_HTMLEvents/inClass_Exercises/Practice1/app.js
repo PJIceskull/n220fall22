a17bb4fbb4d85c21d5573f9d990634f42779f739
Pierce Issah <PJIceskull2003@gmail.com>
2022-11-16 12:44:13 -0500

    wrote code for function

--- 10_HTMLEvents/inClass_Exercises/Practice1/app.js
+++ 10_HTMLEvents/inClass_Exercises/Practice1/app.js
@@ -1,9 +1,9 @@
 // Variables
 let buttons = document.getElementsByTagName("button"); //
-let rock_ID = document.getElementById("rock_ID"); //
-let paper_ID = document.getElementById("paper_ID"); //
-let scissors_ID = document.getElementById("scissors_ID"); //
-printResult = document.getElementById("printResult");
+// let rock_ID = document.getElementById("rock_ID"); //
+// let paper_ID = document.getElementById("paper_ID"); //
+// let scissors_ID = document.getElementById("scissors_ID"); //
+let printResult = document.getElementById("printResult");
 
 let moves = ["Rock", "Paper", "Scissors"]; // Possible moves that are playable
 let moveOutcomes = ["Win", "Tie", "Lose"]; // Arrays that holds possible win outcomes
@@ -14,4 +14,12 @@ for (i = 0; i < buttons.length; i++) {
     buttons[i].style.color = "#000";
 }
 
-function playMove(event) { }
+function playMove(event) {
+    let player = event.target.dataset;
+    console.log(player);
+    if (player == moves.toLowerCase) {
+        console.log("Rock");
+        printResult.innerHTML = "Rock Winds";
+    }
+    // printResult.inn;
+}
