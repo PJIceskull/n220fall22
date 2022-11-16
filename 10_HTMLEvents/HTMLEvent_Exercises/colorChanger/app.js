852c072d02de1fe8b0dc790612128c74b95b7822
Pierce Issah <PJIceskull2003@gmail.com>
2022-11-16 12:06:16 -0500

    write console log

--- 10_HTMLEvents/HTMLEvent_Exercises/colorChanger/app.js
+++ 10_HTMLEvents/HTMLEvent_Exercises/colorChanger/app.js
@@ -1,15 +1,16 @@
 // Variables
-let divSize = 200; // Number to use for the size values of the divs
+let divSize = 200; // Number to use for the size values of the divs 
 // let greyDiv = document.getElementById("greyDiv");
 let greyDiv = document.getElementsByClassName("greyDiv"); // Target Elements with the class, greyDiv
 
-// greyDiv.style.width = divSize + "px";
-// greyDiv.style.height = divSize + "px";
-// greyDiv.style.backgroundColor = "#c0c0c0";
-
+console.log(greyDiv)
 // Use Loop write styles for Divs
 for (i = 0; i < greyDiv.length; i++) {
     greyDiv[i].style.backgroundColor = "#c0c0c0"; //
     greyDiv[i].style.width = divSize + "px";
     greyDiv[i].style.height = divSize + "px";
+}
+
+function divClick() {
+    console.log(greyDiv[i]);
 }
