893640cd5a250a99c36d870fe7574a88bd308999
Pierce Issah <PJIceskull2003@gmail.com>
2022-12-06 12:30:30 -0500

    create an array that hold color values

--- Final_Project/Tic-Tac-Toe/app.js
+++ Final_Project/Tic-Tac-Toe/app.js
@@ -1,2 +1,5 @@
 // Variables
 let divBox = document.getElementsByClassName("box"); // Target elements with "Box" Class
+// let colorPalette = ["#C41E3D", "#7D1128", "#FF2C55", "#3C0919", "#E2294F"];
+// let colorPalette = ["#F05D5E", "#0F7173", "#E7ECEF", "#272932", "#D8A47F"];
+// let colorPalette = ["#3B0D11", "#6A3937", "#706563", "#748386", "#9DC7C8"];
