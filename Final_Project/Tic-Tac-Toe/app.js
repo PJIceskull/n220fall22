9cc9e89db970e3fe8cd87f305cf9695a69b555df
Pierce Issah <PJIceskull2003@gmail.com>
2022-12-06 19:43:04 -0500

    created variables and add comments

--- Final_Project/Tic-Tac-Toe/app.js
+++ Final_Project/Tic-Tac-Toe/app.js
@@ -1,5 +1,7 @@
 // Variables
 let divBox = document.getElementsByClassName("box"); // Target elements with "Box" Class
+let i = 0; // use i as a loop counter
+let boxSize = 100; // Var for div box size. Use one value since width and height should be the same.
 // Array that hold color values
 // let colorPalette = ["#C41E3D", "#7D1128", "#FF2C55", "#3C0919", "#E2294F"];
 // let colorPalette = ["#F05D5E", "#0F7173", "#E7ECEF", "#272932", "#D8A47F"];
