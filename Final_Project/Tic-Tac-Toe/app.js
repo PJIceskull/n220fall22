356bfe6cbbd6b1ced4b5127a6cd4a9bb1ec21359
Pierce Issah <PJIceskull2003@gmail.com>
2022-12-07 14:36:08 -0500

    replace let with const

--- Final_Project/Tic-Tac-Toe/app.js
+++ Final_Project/Tic-Tac-Toe/app.js
@@ -1,13 +1,14 @@
 // Variables
-let divBox = document.getElementsByClassName("box"); // Target elements with "Box" Class
+const divBox = document.getElementsByClassName("box"); // Target elements with "Box" Class
 let i = 0; // use i as a loop counter
 let boxSize = 100; // Var for div box size. Use one value since width and height should be the same.
 // Array that hold color values
-// let colorPalette = ["#C41E3D", "#7D1128", "#FF2C55", "#3C0919", "#E2294F"];
-let colorPalette = ["#F05D5E", "#0F7173", "#E7ECEF", "#272932", "#D8A47F"];
-// let colorPalette = ["#3B0D11", "#6A3937", "#706563", "#748386", "#9DC7C8"];
+// const colorPalette = ["#C41E3D", "#7D1128", "#FF2C55", "#3C0919", "#E2294F"];
+const colorPalette = ["#F05D5E", "#0F7173", "#E7ECEF", "#272932", "#D8A47F"];
+// const colorPalette = ["#3B0D11", "#6A3937", "#706563", "#748386", "#9DC7C8"];
 
 // divBox[i].addEventListener("onclick", onMouseClick);
+
 
 // DOM Loop for targets the divs
 for (i = 0; i < divBox.length; i++) {
@@ -17,8 +18,9 @@ for (i = 0; i < divBox.length; i++) {
     divBox[i].style.width = boxSize + "px";
     divBox[i].style.backgroundColor = colorPalette[2]; // Assign a Color to the divs' bg color using the colorPalette array
     // When one of the divs are clicked
-    divBox[i].onclick = onMouseClick;
+    // divBox[i].onclick = onMouseClick;
 }
+
 
 function onMouseClick() {
     console.log(i);
