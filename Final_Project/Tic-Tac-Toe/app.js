cc53f7e87035426b437efb4c48d218500b238ff2
Pierce Issah <PJIceskull2003@gmail.com>
2022-12-06 19:44:55 -0500

    created loop to target divBox array

--- Final_Project/Tic-Tac-Toe/app.js
+++ Final_Project/Tic-Tac-Toe/app.js
@@ -6,3 +6,7 @@ let boxSize = 100; // Var for div box size. Use one value since width and height
 // let colorPalette = ["#C41E3D", "#7D1128", "#FF2C55", "#3C0919", "#E2294F"];
 // let colorPalette = ["#F05D5E", "#0F7173", "#E7ECEF", "#272932", "#D8A47F"];
 // let colorPalette = ["#3B0D11", "#6A3937", "#706563", "#748386", "#9DC7C8"];
+
+for (i = 0; i < divBox.length; i++) {
+
+}
