e4f4e5f08852510e2402abdb9f36494039d5f00d
Pierce Issah <PJIceskull2003@gmail.com>
2022-12-09 17:14:05 -0500

    creaated selectedDiv var and change color

--- Final_Project/Tic-Tac-Toe/app.js
+++ Final_Project/Tic-Tac-Toe/app.js
@@ -13,17 +13,25 @@ const colorPalette = ["#F05D5E", "#0F7173", "#E7ECEF", "#272932", "#D8A47F"];
 // DOM Loop for targets the divs
 for (i = 0; i < divBoxs.length; i++) {
     console.log(divBoxs[i]); // Print to Console the divBox's index
+
     // Assign the value in boxSize var to the divs' height and width
     divBoxs[i].style.height = boxSize + "px";
     divBoxs[i].style.width = boxSize + "px";
     divBoxs[i].style.backgroundColor = colorPalette[2]; // Assign a Color to the divs' bg color using the colorPalette array
+
     // When one of the divs are clicked
-    divBoxs[i].onclick = onMouseClick;
+    // divBoxs[i].onclick = onMouseClick;
+    divBoxs[i].addEventListener("click", onMouseClick);
 }
 
 
-function onMouseClick() {
-    console.log(i);
+function onMouseClick(event) {
+    // Var for Divs that ws selected
+    selectedDiv = event.target; // Make the selected Div to event.target
+    // Print to consle to the div that was selected
+    // console.log(i);
+    console.log(event.target);
     // console.log(divBox[i]);
     // console.log(divBox[i - 1]);
+    selectedDiv.style.backgroundColor = colorPalette[1]; // Change background to test function
 }
