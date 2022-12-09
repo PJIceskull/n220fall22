826c052605be83428bcae56d31e48fc54fdc012d
Pierce Issah <PJIceskull2003@gmail.com>
2022-12-09 17:22:49 -0500

    turn selectedDiv to global variable

--- Final_Project/Tic-Tac-Toe/app.js
+++ Final_Project/Tic-Tac-Toe/app.js
@@ -6,7 +6,7 @@ let boxSize = 100; // Var for div box size. Use one value since width and height
 // const colorPalette = ["#C41E3D", "#7D1128", "#FF2C55", "#3C0919", "#E2294F"];
 const colorPalette = ["#F05D5E", "#0F7173", "#E7ECEF", "#272932", "#D8A47F"];
 // const colorPalette = ["#3B0D11", "#6A3937", "#706563", "#748386", "#9DC7C8"];
-
+let selectedDiv = "";
 
 // DOM Loop for targets the divs
 for (i = 0; i < divBoxs.length; i++) {
