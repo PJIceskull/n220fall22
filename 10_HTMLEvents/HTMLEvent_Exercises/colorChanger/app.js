d8810385f9d2cb8565a5225c62b8859f7351e7fd
Pierce Issah <PJIceskull2003@gmail.com>
2022-12-12 12:56:07 -0500

    created array for rgb Color values

--- 10_HTMLEvents/HTMLEvent_Exercises/colorChanger/app.js
+++ 10_HTMLEvents/HTMLEvent_Exercises/colorChanger/app.js
@@ -1,7 +1,8 @@
 // Variables
 let divSize = 200; // Number to use for the size values of the divs 
 // let greyDiv = document.getElementById("greyDiv");
-let greyDiv = document.getElementsByClassName("greyDiv"); // Target Elements with the class, greyDiv
+const greyDiv = document.getElementsByClassName("greyDiv"); // Target Elements with the class, greyDiv
+const rgbColors = ["#ff0000", "#009933", "#0000e6"]; // Array for rgb color values
 
 console.log(greyDiv)
 // Use Loop write styles for Divs
@@ -9,6 +10,8 @@ for (i = 0; i < greyDiv.length; i++) {
     greyDiv[i].style.backgroundColor = "#c0c0c0"; //
     greyDiv[i].style.width = divSize + "px";
     greyDiv[i].style.height = divSize + "px";
+
+
 }
 
 function divClick() {
